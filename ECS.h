0e49fa57eed548477f9a546f0b725d56a71d08ca
Kelwan <austin@kelway.ca>
2018-03-17 12:38:35 -0400

    More ECS

--- ECS.h
+++ ECS.h
@@ -12,13 +12,13 @@ class Entity;
 
 using ComponentID = std::size_t;
 
-inline ComponentID getComponentID()
+inline ComponentID getComponentTypeID()
 {
   static ComponentID lastID = 0;
   return lastID++;
 }
 
-template <typename T> inline ComponentID getComponentID() noexcept
+template <typename T> inline ComponentID getComponentTypeID() noexcept
 {
   static ComponentID typeID = getComponentID();
   return typeID();
@@ -28,3 +28,52 @@ constexpr std::size_t maxComponents = 32;
 
 using ComponentBitSet = std::bitset<maxcomponents>;
 using ComponentArray = std::array<Component*, maxComponents>;
+
+class Component{
+public:
+  Entity* entity;
+
+  virtual void init() {}
+  virtual void update() {}
+  virtual void draw() {}
+
+  virtual ~Component() {}
+
+};
+
+class entity
+{
+
+private:
+  bool active = true;
+  std::vector<std::unique_ptr<Component>> components;
+
+  ComponentArray componentArray;
+  ComponentBitSet componentBitSet;
+
+public:
+
+void update()
+{
+  for(auto& c : components) c->update();
+  for(auto& c : components) c->draw();
+}
+
+  void draw() {}
+  bool isActive() const { return active; }
+  void destroy() { active = false; }
+
+  template <typename T> bool hasComponent() const
+  {
+    return component BitSet[getComponentID<T>];
+  }
+
+  template <typename T, typename... TArgs>
+  T& addComponent(TArgs&&... mArgs)
+  {
+    T* c(new T(std::forward<TArgs>(mArgs)...));
+  }
+
+
+
+};
